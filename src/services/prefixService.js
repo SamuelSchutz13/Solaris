14358a1b8affaafb93c7f83b6364f32e8faec29b
SamuelSchutz13 <samuelschutz14@gmail.com>
2023-10-06 19:53:42 -0300

    commit para corrigir o prefixService

--- src/services/prefixService.js
+++ src/services/prefixService.js
@@ -1,63 +1,9 @@
-const { JSON_DIR } = require("../config");
-const fs = require("fs");
+const {PREFIX1, PREFIX2, PREFIX3 } = require("../config");
 
-exports.getPremiumStatus = (memberJid, remoteJid) => {
-    const userFilePath = `${JSON_DIR}/user/${memberJid.split('@')[0]}.json`;
-    const groupFilePath = `${JSON_DIR}/group/${remoteJid}.json`;
-
-    let userPremiumStatus = "Inativo";
-    let groupPremiumStatus = "Inativo";
-
-    try {
-        if (fs.existsSync(userFilePath)) {
-            const userData = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
-
-            if (userData.premium && userData.premium === true) {
-                const premiumExpirationDateString = userData.premiumExpirationDate;
-                const currentDate = new Date();
-                const [, day, month, year, time] = premiumExpirationDateString.match(/(\d+)\/(\d+)\/(\d+) às (\d+:\d+:\d+)/);
-                const [hour, minute, second] = time.split(':');
-                const premiumExpirationDate = new Date(year, month - 1, day, hour, minute, second);
-
-                if (!isNaN(premiumExpirationDate.getTime())) {
-                    const timeDifference = premiumExpirationDate.getTime() - currentDate.getTime();
-                    const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
-
-                    if (daysRemaining > 0) {
-                        userPremiumStatus = `Ativo (Expira em ${daysRemaining} ${daysRemaining === 1 ? 'dia' : 'dias'})`;
-                    } else {
-                        userPremiumStatus = `Expirou (${Math.abs(daysRemaining)} ${Math.abs(daysRemaining) === 1 ? 'dia' : 'dias'} atrás)`;
-                    }
-                }
-            }
-        }
-
-        if (fs.existsSync(groupFilePath)) {
-            const groupData = JSON.parse(fs.readFileSync(groupFilePath, 'utf-8'));
-
-            if (groupData.premium && groupData.premium === true) {
-                const premiumExpirationDateString = groupData.premiumExpirationDate;
-                const currentDate = new Date();
-                const [, day, month, year, time] = premiumExpirationDateString.match(/(\d+)\/(\d+)\/(\d+) às (\d+:\d+:\d+)/);
-                const [hour, minute, second] = time.split(':');
-                const premiumExpirationDate = new Date(year, month - 1, day, hour, minute, second);
-
-                if (!isNaN(premiumExpirationDate.getTime())) {
-                    const timeDifference = premiumExpirationDate.getTime() - currentDate.getTime();
-                    const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
-
-                    if (daysRemaining > 0) {
-                        groupPremiumStatus = `Ativo (Expira em ${daysRemaining} ${daysRemaining === 1 ? 'dia' : 'dias'})`;
-                    } else {
-                        groupPremiumStatus = `Expirou (${Math.abs(daysRemaining)} ${Math.abs(daysRemaining) === 1 ? 'dia' : 'dias'} atrás)`;
-                    }
-                }
-            }
-        }
-
-        return { userPremiumStatus, groupPremiumStatus };
-    } catch (error) {
-        console.error(error);
-        return { userPremiumStatus: "Erro", groupPremiumStatus: "Erro" };
-    }
+function getRandomPrefix()  {
+    const prefixes = [PREFIX1, PREFIX2, PREFIX3];
+    const randomIndex = Math.floor(Math.random() * prefixes.length);
+    return prefixes[randomIndex];
 };
+
+module.exports = { getRandomPrefix };
--- src/services/premiumService.js
+++ src/services/premiumService.js
@@ -1,16 +1,19 @@
 const { JSON_DIR } = require("../config");
 const fs = require("fs");
 
-exports.getPremiumStatus = (memberJid) => {
-    const filePath = `${JSON_DIR}/user/${memberJid.split('@')[0]}.json`;
+exports.getPremiumStatus = (memberJid, remoteJid) => {
+    const userFilePath = `${JSON_DIR}/user/${memberJid.split('@')[0]}.json`;
+    const groupFilePath = `${JSON_DIR}/group/${remoteJid}.json`;
+
+    let userPremiumStatus = "Inativo";
+    let groupPremiumStatus = "Inativo";
 
     try {
-        if (fs.existsSync(filePath)) {
-            const jsonData = fs.readFileSync(filePath, 'utf-8');
-            const data = JSON.parse(jsonData);
+        if (fs.existsSync(userFilePath)) {
+            const userData = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
 
-            if (data.premium) {
-                const premiumExpirationDateString = data.premiumExpirationDate;
+            if (userData.premium && userData.premium === true) {
+                const premiumExpirationDateString = userData.premiumExpirationDate;
                 const currentDate = new Date();
                 const [, day, month, year, time] = premiumExpirationDateString.match(/(\d+)\/(\d+)\/(\d+) às (\d+:\d+:\d+)/);
                 const [hour, minute, second] = time.split(':');
@@ -21,16 +24,40 @@ exports.getPremiumStatus = (memberJid) => {
                     const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
 
                     if (daysRemaining > 0) {
-                        return `Ativo (Expira em ${daysRemaining} ${daysRemaining === 1 ? 'dia' : 'dias'})`;
+                        userPremiumStatus = `Ativo (Expira em ${daysRemaining} ${daysRemaining === 1 ? 'dia' : 'dias'})`;
                     } else {
-                        return `Expirou (${Math.abs(daysRemaining)} ${Math.abs(daysRemaining) === 1 ? 'dia' : 'dias'} atrás)`;
+                        userPremiumStatus = `Expirou (${Math.abs(daysRemaining)} ${Math.abs(daysRemaining) === 1 ? 'dia' : 'dias'} atrás)`;
                     }
                 }
             }
         }
+
+        if (fs.existsSync(groupFilePath)) {
+            const groupData = JSON.parse(fs.readFileSync(groupFilePath, 'utf-8'));
+
+            if (groupData.premium && groupData.premium === true) {
+                const premiumExpirationDateString = groupData.premiumExpirationDate;
+                const currentDate = new Date();
+                const [, day, month, year, time] = premiumExpirationDateString.match(/(\d+)\/(\d+)\/(\d+) às (\d+:\d+:\d+)/);
+                const [hour, minute, second] = time.split(':');
+                const premiumExpirationDate = new Date(year, month - 1, day, hour, minute, second);
+
+                if (!isNaN(premiumExpirationDate.getTime())) {
+                    const timeDifference = premiumExpirationDate.getTime() - currentDate.getTime();
+                    const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
+
+                    if (daysRemaining > 0) {
+                        groupPremiumStatus = `Ativo (Expira em ${daysRemaining} ${daysRemaining === 1 ? 'dia' : 'dias'})`;
+                    } else {
+                        groupPremiumStatus = `Expirou (${Math.abs(daysRemaining)} ${Math.abs(daysRemaining) === 1 ? 'dia' : 'dias'} atrás)`;
+                    }
+                }
+            }
+        }
+
+        return { userPremiumStatus, groupPremiumStatus };
     } catch (error) {
         console.error(error);
+        return { userPremiumStatus: "Erro", groupPremiumStatus: "Erro" };
     }
-
-    return "Inativo"; 
 };
