2b0a75a0e714ee52f871d9d0c67fd97f189457e4
SamuelSchutz13 <samuelschutz14@gmail.com>
2023-10-06 19:45:56 -0300

    novas correções

--- src/services/prefixService.js
+++ src/services/prefixService.js
@@ -1,10 +1,64 @@
-const {PREFIX1, PREFIX2, PREFIX3 } = require("../config");
+const { JSON_DIR } = require("../config");
+const fs = require("fs");
 
-function getRandomPrefix()  {
-    const prefixes = [PREFIX1, PREFIX2, PREFIX3];
-    const randomIndex = Math.floor(Math.random() * prefixes.length);
-    return prefixes[randomIndex];
+exports.getPremiumStatus = (memberJid, groupJid) => {
+    const userFilePath = `${JSON_DIR}/user/${memberJid.split('@')[0]}.json`;
+    const groupNumber = groupJid.split('@')[0];
+    const groupFilePath = `${JSON_DIR}/group/${groupNumber}.json`;
+
+    let userPremiumStatus = "Inativo";
+    let groupPremiumStatus = "Inativo";
+
+    try {
+        if (fs.existsSync(userFilePath)) {
+            const userData = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+
+            if (userData.premium && userData.premium === true) {
+                const premiumExpirationDateString = userData.premiumExpirationDate;
+                const currentDate = new Date();
+                const [, day, month, year, time] = premiumExpirationDateString.match(/(\d+)\/(\d+)\/(\d+) às (\d+:\d+:\d+)/);
+                const [hour, minute, second] = time.split(':');
+                const premiumExpirationDate = new Date(year, month - 1, day, hour, minute, second);
+
+                if (!isNaN(premiumExpirationDate.getTime())) {
+                    const timeDifference = premiumExpirationDate.getTime() - currentDate.getTime();
+                    const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
+
+                    if (daysRemaining > 0) {
+                        userPremiumStatus = `Ativo (Expira em ${daysRemaining} ${daysRemaining === 1 ? 'dia' : 'dias'})`;
+                    } else {
+                        userPremiumStatus = `Expirou (${Math.abs(daysRemaining)} ${Math.abs(daysRemaining) === 1 ? 'dia' : 'dias'} atrás)`;
+                    }
+                }
+            }
+        }
+
+        if (fs.existsSync(groupFilePath)) {
+            const groupData = JSON.parse(fs.readFileSync(groupFilePath, 'utf-8'));
+
+            if (groupData.premium && groupData.premium === true) {
+                const premiumExpirationDateString = groupData.premiumExpirationDate;
+                const currentDate = new Date();
+                const [, day, month, year, time] = premiumExpirationDateString.match(/(\d+)\/(\d+)\/(\d+) às (\d+:\d+:\d+)/);
+                const [hour, minute, second] = time.split(':');
+                const premiumExpirationDate = new Date(year, month - 1, day, hour, minute, second);
+
+                if (!isNaN(premiumExpirationDate.getTime())) {
+                    const timeDifference = premiumExpirationDate.getTime() - currentDate.getTime();
+                    const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
+
+                    if (daysRemaining > 0) {
+                        groupPremiumStatus = `Ativo (Expira em ${daysRemaining} ${daysRemaining === 1 ? 'dia' : 'dias'})`;
+                    } else {
+                        groupPremiumStatus = `Expirou (${Math.abs(daysRemaining)} ${Math.abs(daysRemaining) === 1 ? 'dia' : 'dias'} atrás)`;
+                    }
+                }
+            }
+        }
+
+        return { userPremiumStatus, groupPremiumStatus };
+    } catch (error) {
+        console.error(error);
+        return { userPremiumStatus: "Erro", groupPremiumStatus: "Erro" };
+    }
 };
-
-module.exports = { getRandomPrefix };
-
