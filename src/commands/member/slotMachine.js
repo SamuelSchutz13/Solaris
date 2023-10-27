5397278f413cf22bbe81949ed304023ce067c261
SamuelSchutz13 <samuelschutz14@gmail.com>
2023-10-27 11:05:19 -0300

    agora serão gastos 2 solarcoins para jogar na maquina

--- src/commands/member/slotMachine.js
+++ src/commands/member/slotMachine.js
@@ -9,22 +9,36 @@ module.exports = {
         sendSuccessReact, 
         sendReply,
         sendWaitReply,
+        baileysMessage,
     }) => {
-        const simbolos = ["🍒", "🍋", "🍊", "🍇", "🍉", "🍓", "🍍", "🔔", "💎", "7️⃣"];
+        const memberToNameJid = toUserJid(baileysMessage?.key?.participant);
+        const memberNameNumber = onlyNumbers(memberToNameJid);
 
-        const bobina1 = simbolos[Math.floor(Math.random() * simbolos.length)];
-        const bobina2 = simbolos[Math.floor(Math.random() * simbolos.length)];
-        const bobina3 = simbolos[Math.floor(Math.random() * simbolos.length)];
-
-        let resultado = "Você perdeu";
-        if (bobina1 === bobina2 && bobina1 === bobina3) {
-            resultado = "Você ganhou";
-        }
-        
-        await sendWaitReply();
-        await sendSuccessReact();
-        await sendReply(`
-
+        const userJsonPath = `${JSON_DIR}/user/${memberNameNumber}.json`;
+    
+        if (fs.existsSync(userJsonPath)) {
+            try {
+                const jsonData = fs.readFileSync(userJsonPath, 'utf-8');
+                const userData = JSON.parse(jsonData);
+    
+                if (userData.coins >= 2) {
+                    userData.coins -= 2;
+    
+                    fs.writeFileSync(userJsonPath, JSON.stringify(userData, null, 2));
+    
+                    const simbolos = ["🍒", "🍋", "🍊", "🍇", "🍉", "🍓", "🍍", "🔔", "💎", "7️⃣"];
+                    const bobina1 = simbolos[Math.floor(Math.random() * simbolos.length)];
+                    const bobina2 = simbolos[Math.floor(Math.random() * simbolos.length)];
+                    const bobina3 = simbolos[Math.floor(Math.random() * simbolos.length)];
+    
+                    let resultado = "Você perdeu";
+                    if (bobina1 === bobina2 && bobina1 === bobina3) {
+                        resultado = "Você ganhou";
+                    }
+    
+                    await sendWaitReply();
+                    await sendSuccessReact();
+                    await sendReply(`
 ╭─「🎰 CAÇA NÍQUEL 🎰」
 │
 │─「${bobina1}」「${bobina2}」「${bobina3}」─
@@ -32,6 +46,20 @@ module.exports = {
 │─ ${resultado}
 │ 
 ╰─────────────────
-        `);
+
+Você gastou 2 solarcoins. Agora você tem ${userData.coins} solarcoins em sua conta 
+                    `);
+                } else {
+                    await sendWaitReply();
+                    await sendReply("Você não tem solarcoins suficientes para jogar na máquina caça-níquel");
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        } else {
+            await sendWaitReply();
+            await sendReply("Você não está registrado como um usuário");
+        }
     },
+    
 };
