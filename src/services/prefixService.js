c2e0a1f40d4ae2cea64e3a0572d1d8a2d07b9988
SamuelSchutz13 <samuelschutz14@gmail.com>
2023-11-25 12:19:49 -0300

    correções na qual adicionada mais um prefixo ao bot

--- src/services/prefixService.js
+++ src/services/prefixService.js
@@ -1,7 +1,7 @@
-const {PREFIX1, PREFIX2, PREFIX3 } = require("../config");
+const {PREFIX1, PREFIX2, PREFIX3, PREFIX4 } = require("../config");
 
 function getRandomPrefix()  {
-    const prefixes = [PREFIX1, PREFIX2, PREFIX3];
+    const prefixes = [PREFIX1, PREFIX2, PREFIX3, PREFIX4];
     const randomIndex = Math.floor(Math.random() * prefixes.length);
     return prefixes[randomIndex];
 };
