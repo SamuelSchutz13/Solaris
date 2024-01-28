63654f8b00f9cd4a7fd1a48aa20cb64ab5b7e24a
Samuel_Schutz <140556072+SamuelSchutz13@users.noreply.github.com>
2024-01-28 19:14:22 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,11 +8,11 @@ exports.PREFIX4 = "!";
 exports.BOT_EMOJI = "🤖";
 exports.BOT_VERSION = "1.0.2";
 exports.BOT_NAME = "Solaris Bot";
-exports.BOT_NUMBER = "37259022954";
+exports.BOT_NUMBER = "";
 
-exports.DEV_ID = "554999314419@s.whatsapp.net";
-exports.PIX_KEY="153eb01f-d257-4238-a764-8a8c058f52c5";    
-exports.DEV_NAME = "Samuel";
+exports.DEV_ID = "";
+exports.PIX_KEY="";    
+exports.DEV_NAME = "";
 
 exports.COMMANDS_DIR = path.join(__dirname, "commands");
 exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
@@ -25,8 +25,8 @@ exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
 exports.PAGES = 10; 
 exports.COINS_COST = 5;
 
-exports.GPT_API_KEY = "pk-MArOXaCaFgKfKbksrTzAQsOrCBXpjArdhHUwBqcVxrSIpBDx";
-exports.OPENWEATHER_API_KEY = "7d751384326d2e9757e6549a5ef56a2b";
-exports.NEWS_API_KEY="9c94542378104c0b8f23fdc561985265";
-exports.API_PRO_KEY="$2y$10$pQ/coy3DlCahE";
-exports.STICKER_API_KEY="c62ba06751e25d258a0c5a8dceffe821213a1b43"; 
+exports.GPT_API_KEY = "";
+exports.OPENWEATHER_API_KEY = "";
+exports.NEWS_API_KEY="";
+exports.API_PRO_KEY="";
+exports.STICKER_API_KEY="; 
